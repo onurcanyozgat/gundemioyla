cf72270495600883f115e9af843c43263af2f52b
onurcanyozgat <onurcanyozgat@gmail.com>
2023-02-22 00:06:01 +0300

    Initial commit

---
+++ src/main/java/com/quip/gundemioyla/GundemioylaApplication.java
@@ -0,0 +1,13 @@
+package com.quip.gundemioyla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GundemioylaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GundemioylaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quip/gundemioyla/GundemioylaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quip.gundemioyla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GundemioylaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
