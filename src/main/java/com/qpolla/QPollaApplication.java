fa875d085a66a0a2e20494c8b60b30ec92095ad7
volkan.ulutas <volkan.ulutas@esensi.com.tr>
2023-03-01 09:52:58 +0300

    initial

---
+++ src/main/java/com/qpolla/QPollaApplication.java
@@ -0,0 +1,11 @@
+package com.qpolla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QPollaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QPollaApplication.class, args);
+    }
+}
--- src/test/java/com/qpolla/GundemioylaApplicationTests.java
+++ src/test/java/com/qpolla/GundemioylaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.quip.gundemioyla;
+package com.qpolla;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
