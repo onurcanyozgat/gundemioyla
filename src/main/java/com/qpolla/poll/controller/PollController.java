4b830e327b1819bc2ee63ae9c52d3d4fbe66d9a9
onurcan.yozgat <onurcan.yozgat@esensi.com.tr>
2023-03-02 13:37:16 +0300

    controller structure created

---
+++ src/main/java/com/qpolla/poll/controller/PollController.java
@@ -0,0 +1,14 @@
+package com.qpolla.poll.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@Slf4j
+@RequestMapping("api/poll")
+public class PollController {
+
+}
