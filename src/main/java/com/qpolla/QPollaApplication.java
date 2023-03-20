3ff80f4f4ce905f20c11c73e52ef904574085c2a
Volkan Ulutaş <volkanulutas@hacettepe.edu.tr>
2023-03-20 23:44:35 +0300

    converter

--- src/main/java/com/qpolla/QPollaApplication.java
+++ src/main/java/com/qpolla/QPollaApplication.java
@@ -2,10 +2,25 @@ package com.qpolla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.beans.BeanProperty;
 
 @SpringBootApplication
 public class QPollaApplication {
     public static void main(String[] args) {
         SpringApplication.run(QPollaApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry reg) {
+                reg.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
